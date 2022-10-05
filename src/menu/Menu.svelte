0c32798aadcefae6eab168110f1528c33485c258
Ruben Baas <defichain-trader@tutanota.com>
2022-10-05 04:23:18 +0200

    Use unresponsive horizontal menu instead of the responsive vertical one

--- src/menu/Menu.svelte
+++ src/menu/Menu.svelte
@@ -28,7 +28,7 @@
 </a>
 
 <div id="menu">
-    <div class="pure-menu">
+    <div class="pure-menu pure-menu-horizontal">
         <ul class="pure-menu-list">
             <li class="pure-menu-item logo">
                 <img src="logo.png" width="140" height="38"/>
@@ -91,129 +91,5 @@
 </div>
 
 <style>
-    .logo {
-        display: flex;
-        justify-content: center;
-        padding-top: 1rem;
-        padding-bottom: 1rem;
-    }
-    /*
-    The `#menu` `<div>` is the parent `<div>` that contains the `.pure-menu` that
-    appears on the left side of the page.
-    */
 
-    #menu {
-        margin-left: -150px; /* "#menu" width */
-        width: 150px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        z-index: 1000; /* so the menu or its navicon stays above all content */
-        background: #191818;
-        overflow-y: auto;
-    }
-
-    /*
-    All anchors inside the menu should be styled like this.
-    */
-    #menu a {
-        color: #999;
-        border: none;
-        padding: 0.6em 0 0.6em 0.6em;
-    }
-
-    /*
-    Remove all background/borders, since we are applying them to #menu.
-    */
-    #menu .pure-menu,
-    #menu .pure-menu ul {
-        border: none;
-        background: transparent;
-    }
-
-    /*
-    Add that light border to separate items into groups.
-    */
-    #menu .pure-menu ul,
-    #menu .pure-menu .menu-item-divided {
-        border-top: 1px solid #333;
-    }
-
-    /*
-    Change color of the anchor links on hover/focus.
-    */
-    #menu .pure-menu li a:hover,
-    #menu .pure-menu li a:focus {
-        background: #333;
-    }
-
-    /*
-    This styles the selected menu item `<li>`.
-    */
-    #menu .pure-menu-selected {
-        background: #1f8dd6;
-    }
-
-    /*
-    This styles a link within a selected menu item `<li>`.
-    */
-    #menu .pure-menu-selected a {
-        color: #fff;
-    }
-
-    /* -- Dynamic Button For Responsive Menu -------------------------------------*/
-
-    /*
-    The button to open/close the Menu is custom-made and not part of Pure. Here's
-    how it works:
-    */
-
-    /*
-    `.menu-link` represents the responsive menu toggle that shows/hides on
-    small screens.
-    */
-    .menu-link {
-        position: fixed;
-        display: block; /* show this only on small screens */
-        top: 0;
-        left: 0; /* "#menu width" */
-        background: #000;
-        background: rgba(0, 0, 0, 0.7);
-        font-size: 10px; /* change this value to increase/decrease button size */
-        z-index: 10;
-        width: 2em;
-        height: auto;
-        padding: 2.1em 1.6em;
-    }
-
-    .menu-link:hover,
-    .menu-link:focus {
-        background: #000;
-    }
-
-    .menu-link span {
-        position: relative;
-        display: block;
-    }
-
-    .menu-link span,
-    .menu-link span:before,
-    .menu-link span:after {
-        background-color: #fff;
-        pointer-events: none;
-        width: 100%;
-        height: 0.2em;
-    }
-
-    .menu-link span:before,
-    .menu-link span:after {
-        position: absolute;
-        margin-top: -0.6em;
-        content: " ";
-    }
-
-    .menu-link span:after {
-        margin-top: 0.6em;
-    }
 </style>
