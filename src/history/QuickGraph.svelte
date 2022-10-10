4bcbd639925ba512ad368b9baa6a4615b2749664
Ruben Baas <defichain-trader@tutanota.com>
2022-10-10 04:05:33 +0200

    Default to DFI to BTC

--- src/history/QuickGraph.svelte
+++ src/history/QuickGraph.svelte
@@ -1,16 +1,20 @@
 <script>
     import FromToTokenFilter from "../dex/FromToTokenFilter.svelte";
     import PoolSwapGraph from "../dex/PoolSwapGraph.svelte";
+    import {onMount} from "svelte";
 
     export let allTokens
     export let Chart
 
-    let fromTokenSymbol
-    let toTokenSymbol
+    let fromTokenSymbol = 'DFI'
+    let toTokenSymbol = 'BTC'
     let poolSwap
     let estimates
 
     async function update() {
+        estimates = null
+        poolSwap = null
+
         if (fromTokenSymbol && toTokenSymbol && fromTokenSymbol !== 'Any' && toTokenSymbol !== 'Any') {
             poolSwap = {
                 tokenFrom: fromTokenSymbol,
@@ -27,6 +31,8 @@
         toTokenSymbol = selection.toTokenSymbol
         await update()
     }
+
+    onMount(async () => await update())
 </script>
 
 <FromToTokenFilter supportAnyToken={true}
