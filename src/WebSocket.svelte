64c4dee431dc6e1debbc80f18be0fde21a5efd6e
Ruben Baas <defichain-trader@tutanota.com>
2022-10-23 23:45:41 +0200

    Only show websocket status when disconnected or connecting

--- src/WebSocket.svelte
+++ src/WebSocket.svelte
@@ -1,6 +1,7 @@
 <script>
     import {addPoolswap, incomingMessages, outgoingMessages} from "./store";
 
+    let connecting = true
     let socket
     let connected
     let error
@@ -27,6 +28,7 @@
     })
 
     const newSocket = () => {
+        connecting = true
         if (socket) {
             socket.onclose = function () {
             }
@@ -42,6 +44,7 @@
         }
 
         socket.onopen = () => {
+            connecting = false
             connected = true
             error = null
             socket.send(JSON.stringify({
@@ -57,6 +60,7 @@
         }
 
         socket.onclose = function () {
+            connecting = false
             error = 'You are not connected to the server'
             connected = false
         }
@@ -70,15 +74,31 @@
     newSocket()
 </script>
 
-<div>
-    <label>
-        Status
-        {#if connected}
-            Connected
-        {:else}
+{#if connecting}
+    Connecting...
+{:else if !connected}
+    <section>
+        <div>
+            <h1>You are not connected!</h1>
+            <p>Make sure you are connected to the internet.</p>
+            <p>It is also possible that the servers are restarting to deploy an update.</p>
+            <p>You may have encountered a bug that caused you to be disconnected. Or you could be using an unsupported
+                device. Contact the maintainer if the problem
+                persists.</p>
             <button class="pure-button" on:click={newSocket}>Reconnect</button>
-        {/if}
-    </label>
-</div>
+        </div>
+    </section>
+{/if}
+
+<style>
+    section {
+        padding: 1rem;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background: white;
+        z-index: 100;
+    }
+</style>
 
 
