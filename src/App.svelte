0cac3e2408fea120b61489c4de0bbe2915b252b9
Ruben Baas <defichain-trader@tutanota.com>
2022-10-24 20:48:32 +0200

    Add account management placeholder

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,13 @@
     import Menu from "./menu/Menu.svelte";
     import WebSocket from "./WebSocket.svelte";
     import Stats from "./stats/Stats.svelte";
+    import Account from "./account/Account.svelte";
 
     let allTokens
     let error
     let Chart
     let mainElement
-    let componentType = Stats
+    let componentType = Account
 
     window.fireChartJSLoad = chartConstructor => {
         Chart = chartConstructor
---
+++ src/account/Account.svelte
@@ -0,0 +1,48 @@
+<script>
+    const options = [
+        {label: 'Pool Swaps'},
+        {label: 'Liquidity'},
+        {label: 'Loans'},
+        {label: 'Collateral'},
+    ]
+</script>
+
+<div class="pure-menu">
+    <span class="pure-menu-heading">My strategy</span>
+    <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+            <a href="#poolswaphistory" class="pure-menu-link">Limit orders</a>
+        </li>
+    </ul>
+</div>
+
+<div class="pure-menu">
+    <span class="pure-menu-heading">My filters</span>
+    <ul class="pure-menu-list">
+        {#each options as option}
+            <li class="pure-menu-item">
+                <a href="#poolswaphistory" class="pure-menu-link">{option.label}</a>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<div class="pure-menu">
+    <span class="pure-menu-heading">My notifications</span>
+    <ul class="pure-menu-list">
+
+        <li class="pure-menu-item">
+            {#each options as option}
+                <li class="pure-menu-item">
+                    <a href="#poolswaphistory" class="pure-menu-link">{option.label}</a>
+                </li>
+            {/each}
+        </li>
+    </ul>
+</div>
+
+<style>
+    .pure-menu-heading {
+        font-weight: bold;
+    }
+</style>
