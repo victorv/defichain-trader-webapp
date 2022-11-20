10889598d03e1ac2d541efb86abe050fb0e7d97d
Ruben Baas <defichain-trader@tutanota.com>
2022-11-20 20:34:54 +0100

    Remove obsolete code

--- src/account/Account.svelte
+++ src/account/Account.svelte
@@ -13,15 +13,6 @@
     ]
 </script>
 
-<!--<div class="pure-menu">-->
-<!--    <span class="pure-menu-heading">My strategy</span>-->
-<!--    <ul class="pure-menu-list">-->
-<!--        <li class="pure-menu-item">-->
-<!--            <a href="#poolswaphistory" class="pure-menu-link">Limit orders</a>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</div>-->
-
 {#if !componentType}
     <div class="pure-menu">
         <span class="pure-menu-heading">My filters</span>
@@ -42,20 +33,6 @@
 {#if componentType}
     <svelte:component this={componentType}/>
 {/if}
-
-<!--<div class="pure-menu">-->
-<!--    <span class="pure-menu-heading">My notifications</span>-->
-<!--    <ul class="pure-menu-list">-->
-
-<!--        <li class="pure-menu-item">-->
-<!--            {#each options as option}-->
-<!--                <li class="pure-menu-item">-->
-<!--                    <a href="#poolswaphistory" class="pure-menu-link">{option.label}</a>-->
-<!--                </li>-->
-<!--            {/each}-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</div>-->
 
 <style>
     .pure-menu-heading {
