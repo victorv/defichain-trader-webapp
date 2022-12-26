dcb8a0e5c189f7c129931bcca362c6682b55ae6c
Ruben Baas <defichain-trader@tutanota.com>
2022-12-27 00:03:11 +0100

    Update status page

--- src/menu/StatusPage.svelte
+++ src/menu/StatusPage.svelte
@@ -1,3 +1,3 @@
 <p>
-    Mempool and Telegram notifications were down for the most part between 11/26/2022 22:00 UTC+1:00 and 12/26/2022 16:30 UTC+1:00 because of an issue with one of the nodes
+    Telegram notifications and mempool are down because of an issue that is currently being investigated
 </p>
