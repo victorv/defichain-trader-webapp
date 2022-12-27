fc433d73a00a18044c3b88371e426e3023d61137
Ruben Baas <defichain-trader@tutanota.com>
2022-12-27 03:29:39 +0100

    Update status page

--- src/menu/StatusPage.svelte
+++ src/menu/StatusPage.svelte
@@ -1,3 +1,5 @@
 <p>
-    Telegram notifications and mempool are down because of an issue that is currently being investigated
+    Due to a client crash related to transactions that updated master nodes the website experienced delays and outages over the past few
+    days. A workaround is in place until a new version of the DeFiChain full node is released. The website and Telegram
+    notifications are fully functional again.
 </p>
