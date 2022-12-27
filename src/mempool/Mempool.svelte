9f614c02766211c0cd1742992f84af9d5db3f0b9
Ruben Baas <defichain-trader@tutanota.com>
2022-12-27 04:25:33 +0100

    Remove message

--- src/mempool/Mempool.svelte
+++ src/mempool/Mempool.svelte
@@ -20,8 +20,6 @@
 {#if !items || !items.length}
     <div class="container">
         <div>
-            <p>Make a cup of tea, relax and watch transactions that were just announced come in. Maybe you will even see
-                your own transaction here shortly after you have submitted it from your wallet.</p>
             <span class="info">Keep open to receive transactions. Mempool resets when a block is minted and can be empty.</span>
         </div>
     </div>
