2c53fb7a1b61c61cee2387f62f150ce8027f93e4
Ruben Baas <defichain-trader@tutanota.com>
2023-01-18 15:56:48 +0100

    Correct for duplicate times

--- src/dex/TradeChart.svelte
+++ src/dex/TradeChart.svelte
@@ -54,13 +54,20 @@
 
     const createCandles = () => {
         const candles = []
+        let prevTime = 0
         for (const candleEntry of graph) {
+            let time = candleEntry[4]
+            if (time === prevTime) {
+                time += 1
+            }
+            prevTime = time
+
             candles.push({
                 open: candleEntry[0],
                 close: candleEntry[1],
                 low: candleEntry[2],
                 high: candleEntry[3],
-                time: candleEntry[4],
+                time,
             })
         }
         return candles
